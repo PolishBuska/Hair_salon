28d0207feec8b7afd54b8eda0b7e191d4564b9ae
Stas <375292765815@mail.ru>
2024-01-30 23:16:14 +0300

    Fixed logger's logging multiple times

--- infrastructure/loggers/logger.py
+++ infrastructure/loggers/logger.py
@@ -6,11 +6,16 @@ class Logger:
     def __init__(self, name, level):
         self.level = level
         self.logger = logging.getLogger(name=name)
+        self.logger.propagate = False
         self.logger.setLevel(level=level)
         log_file = os.path.join('logs', f'{name}_{name.lower()}.txt')
         handler = logging.FileHandler(log_file)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
+
+        if self.logger.hasHandlers():
+            self.logger.handlers.clear()
+
         self.logger.addHandler(handler)
 
     def msg(self, message: str):
